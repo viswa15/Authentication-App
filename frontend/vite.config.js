59be115c409054d3a2877dc9a0d89855844e2403
viswa15 <viswatejatummala@gmail.com>
2025-02-22 12:44:59 +0530

    final-8

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',  // Add this line
+  base: '/',  // Add this line
   plugins: [react()],
   outDir: 'dist', // Ensure this is set correctly
   assetsDir: 'assets', // Ensure this is set
